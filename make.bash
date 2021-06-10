1e37a8536fbed37d3d099009ee15254a3b949497
shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2021-06-11 00:38:10 +0900

    Add missing header files to Makefile.am and autoreconf

--- make.bash
+++ make.bash
@@ -3,12 +3,16 @@ set -euo pipefail
 export LC_ALL=C
 export LANG=C
 
+ISOLA_HOME=${ISOLA_HOME:-${HOME}}
+INSTALL_DIR=${INSTALL_DIR:-${ISOLA_HOME}/opt/massivethreads-dm}
+
 CFLAGS=""
 # CFLAGS="-g"
-CFLAGS="-O0 -g"
+# CFLAGS="-O0 -g"
 
 # CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure
-CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3
+CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --prefix=${INSTALL_DIR}
 
 make clean
 make -j
+make install
