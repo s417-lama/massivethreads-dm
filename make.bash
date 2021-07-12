4e355c80aa0d21ed05bce0f538a705995edce146
shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2021-07-13 00:13:15 +0900

    update scripts (add missing files)

--- make.bash
+++ make.bash
@@ -3,15 +3,21 @@ set -euo pipefail
 export LC_ALL=C
 export LANG=C
 
+source scripts/envs.bash
+
 ISOLA_HOME=${ISOLA_HOME:-${HOME}}
 INSTALL_DIR=${INSTALL_DIR:-${ISOLA_HOME}/opt/massivethreads-dm}
 
 CFLAGS=""
 # CFLAGS="-g"
-# CFLAGS="-O0 -g"
 
 # CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure
 CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --prefix=${INSTALL_DIR}
+
+# debug
+# DEBUG_LEVEL=0
+# CFLAGS="$CFLAGS -O0 -g"
+# CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --with-madi-debug-level=$DEBUG_LEVEL --prefix=${INSTALL_DIR}
 
 make clean
 make -j
