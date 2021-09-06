596857891cc16981bcf552494b136d67aeebc989
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2021-09-06 18:27:23 +0900

    disable copying gcc to /tmp on ITO

--- scripts/envs.bash
+++ scripts/envs.bash
@@ -20,21 +20,12 @@ if [[ $(hostname) =~ "obcx" ]]; then
 elif [[ $(hostname) =~ "ito" ]] || [[ $(hostname) =~ "sca" ]]; then
   export MACHINE_NAME=ito
 
-  # module load gcc/10.2.0
-  GCC_DIR=/tmp/$USER/gcc
-  if [[ ! -d $GCC_DIR ]]; then
-    # create a local cache of gcc
-    # because access to /home is too slow on ITO
-    mkdir -p $GCC_DIR
-    cp -r /home/app/gcc/10.2.0/* $GCC_DIR
-  fi
-  export PATH=$GCC_DIR/bin:$PATH
-  export LD_LIBRARY_PATH=$GCC_DIR/lib64:$LD_LIBRARY_PATH
-
   # export PATH=$HOME/opt/openmpi/bin:$PATH
   # export LD_LIBRARY_PATH=$HOME/opt/openmpi/lib:$LD_LIBRARY_PATH
   export PATH=$HOME/opt/openmpi5/bin:$PATH
   export LD_LIBRARY_PATH=$HOME/opt/openmpi5/lib:$LD_LIBRARY_PATH
+
+  module load gcc/10.2.0
 
 else
   export MACHINE_NAME=local
