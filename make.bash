1ed028f43057a7e402047e731c116fe608506fc3
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2021-12-10 16:05:36 +0900

    Update make script to support old bash version

--- make.bash
+++ make.bash
@@ -26,7 +26,7 @@ logger_options=() # logger disabled
 # logger_options=(--enable-logger --disable-logger-trace --with-logger-enabled-kinds="$enabled_kinds" --with-logger-disabled-kinds="$disabled_kinds") # stats
 
 if [[ "$MACHINE_NAME" == ito ]]; then
-  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 "${logger_options[@]}" --prefix=${INSTALL_DIR}
+  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 ${logger_options[@]+"${logger_options[@]}"} --prefix=${INSTALL_DIR}
 
   # debug
   # DEBUG_LEVEL=3
@@ -34,14 +34,14 @@ if [[ "$MACHINE_NAME" == ito ]]; then
   # CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --with-madi-debug-level=$DEBUG_LEVEL --prefix=${INSTALL_DIR}
 elif [[ "$MACHINE_NAME" == wisteria-o ]]; then
   CFLAGS="$CFLAGS -Nclang"
-  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure MPICC=mpifccpx MPICXX=mpiFCCpx --host=aarch64 --with-comm-layer=mpi3 "${logger_options[@]}" --prefix=${INSTALL_DIR}
+  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure MPICC=mpifccpx MPICXX=mpiFCCpx --host=aarch64 --with-comm-layer=mpi3 ${logger_options[@]+"${logger_options[@]}"} --prefix=${INSTALL_DIR}
 
   # debug
   # DEBUG_LEVEL=3
   # CFLAGS="$CFLAGS -O0 -g"
   # CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure MPICC=mpifccpx MPICXX=mpiFCCpx --host=aarch64 --with-comm-layer=mpi3 --with-madi-debug-level=$DEBUG_LEVEL --prefix=${INSTALL_DIR}
 else
-  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --enable-polling "${logger_options[@]}" --prefix=${INSTALL_DIR}
+  CCFLAGS=$CFLAGS CXXFLAGS=$CFLAGS ./configure --with-comm-layer=mpi3 --enable-polling ${logger_options[@]+"${logger_options[@]}"} --prefix=${INSTALL_DIR}
 
   # debug
   # DEBUG_LEVEL=3
